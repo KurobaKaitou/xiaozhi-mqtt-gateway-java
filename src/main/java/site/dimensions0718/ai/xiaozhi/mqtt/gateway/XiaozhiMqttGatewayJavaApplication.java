1302f85a302e681ce7497faba37bf58fae51fcb6
小雨 <1537372358@qq.com>
2026-03-16 10:39:52 +0800

    feat(infra): initialize the project

---
+++ src/main/java/site/dimensions0718/ai/xiaozhi/mqtt/gateway/XiaozhiMqttGatewayJavaApplication.java
@@ -0,0 +1,13 @@
+package site.dimensions0718.ai.xiaozhi.mqtt.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaozhiMqttGatewayJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhiMqttGatewayJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/dimensions0718/ai/xiaozhi/mqtt/gateway/XiaozhiMqttGatewayJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dimensions0718.ai.xiaozhi.mqtt.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaozhiMqttGatewayJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
